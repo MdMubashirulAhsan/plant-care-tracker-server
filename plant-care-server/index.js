46503a998e4a907e2fb31e8f3bfd18a2badf167e
MdMubashirulAhsan <ahsan.mubashirul@gmail.com>
2025-05-21 19:33:38 +0600

    added api to update data on mongodb

--- plant-care-server/index.js
+++ plant-care-server/index.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app =express();
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uf8t3so.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,105 +24,121 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-        const plantCollection = client.db('plantDB').collection('plants');
-        const usersCollection = client.db('plantDB').collection('users');
+    const plantCollection = client.db("plantDB").collection("plants");
 
-        app.get('/plants', async (req, res) => {
-            // const cursor = coffeesCollection.find();
-            
-            const result = await plantCollection.find().toArray();
-            res.send(result);
-        });
+    const usersCollection = client.db("plantDB").collection("users");
+    // const slidesCollection = client.db('plantDB').collection('slides');
+    // const dummyCollection = client.db('plantDB').collection('users');
 
-        app.get('/plants/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await plantCollection.findOne(query);
-            res.send(result);
-        })
+    app.get("/plants", async (req, res) => {
+      // const cursor = coffeesCollection.find();
 
-        app.post('/plants', async (req, res) => {
-            const newPlant = req.body;
-            console.log(newPlant)
-            const result = await plantCollection.insertOne(newPlant);
-            res.send(result);
-        })
+      const result = await plantCollection
+        .find()
 
-        app.put('/plants/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true };
-            const updatedPlant = req.body;
-            const updatedDoc = {
-                $set: updatedPlant
-            }
+        .toArray();
 
-            // const updatedDoc = {
-            //     $set: {
-            //         name: updatedCoffee.name, 
-            //         supplier: updatedCoffee.supplier
-            //     }
-            // }
+      res.send(result);
+    });
+    // app.get('/slides', async (req, res) => {
+    //     // const cursor = coffeesCollection.find();
 
-            const result = await plantCollection.updateOne(filter, updatedDoc, options);
+    //     const result = await slidesCollection.find().toArray();
+    //     res.send(result);
+    // });
+    // app.get('/dummy', async (req, res) => {
+    //     // const cursor = coffeesCollection.find();
 
-            res.send(result);
-        })
+    //     const result = await dummyCollection.find().toArray();
+    //     res.send(result);
+    // });
 
+    app.get("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantCollection.findOne(query);
+      res.send(result);
+    });
 
-        app.delete('/plants/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await plantCollection.deleteOne(query);
-            res.send(result);
-        })
+    app.post("/plants", async (req, res) => {
+      const newPlant = req.body;
+      console.log(newPlant);
+      const result = await plantCollection.insertOne(newPlant);
+      res.send(result);
+    });
 
+    app.put("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedPlant = req.body;
+      const updatedDoc = {
+        $set: updatedPlant,
+      };
 
-        // User related APIs
-        app.get('/users', async (req, res) => {
-            const result = await usersCollection.find().toArray();
-            res.send(result);
-        })
+      // const updatedDoc = {
+      //     $set: {
+      //         name: updatedCoffee.name,
+      //         supplier: updatedCoffee.supplier
+      //     }
+      // }
 
+      const result = await plantCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
 
-        app.post('/users', async (req, res) => {
-            const userProfile = req.body;
-            console.log(userProfile)
-            const result = await usersCollection.insertOne(userProfile);
-            res.send(result);
-        })
+      res.send(result);
+    });
 
-        app.patch('/users', async(req, res) =>{
-            const {email, lastSignInTime} = req.body;
-            const filter = {email: email}
-            const updatedDoc = {
-                $set: {
-                    lastSignInTime: lastSignInTime
-                }
-            }
+    app.delete("/plants/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantCollection.deleteOne(query);
+      res.send(result);
+    });
 
-            const result = await usersCollection.updateOne(filter, updatedDoc)
-            res.send(result);
-        })
+    // User related APIs
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-        app.delete('/users/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await usersCollection.deleteOne(query);
-            res.send(result);
-        })
+    app.post("/users", async (req, res) => {
+      const userProfile = req.body;
+      console.log(userProfile);
+      const result = await usersCollection.insertOne(userProfile);
+      res.send(result);
+    });
 
+    app.patch("/users", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime,
+        },
+      };
 
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+  } finally {
+  }
 }
 run().catch(console.dir);
 
-app.listen(port, ()=>{
-    console.log(`Server running on: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server running on: ${port}`);
+});
